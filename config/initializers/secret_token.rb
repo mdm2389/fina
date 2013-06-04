9ed1e73eda0aaec2d81ec5d63915d54ada74794d
Melissa Meade <mdm2389@gmail.com>
2013-06-03 20:12:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fina::Application.config.secret_token = 'f2840710b6b847210deddc42fc2a858d364e78bba08bc8579115e04d21b35f3e3ce65c8795746504f47be930994f6369e4b48a9f800807dd548fb8c391d8913d'
