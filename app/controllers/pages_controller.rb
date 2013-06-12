82c6f15a96119fad93a5e5fc9add6478caeb5e81
Melissa Meade <mdm2389@gmail.com>
2013-06-11 20:02:14 -0400

    Add jQuery Masonry

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
   end
 
-  def action 
+  def about
+  	@pins = Pin.order("created_at desc") 
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Fina::Application.routes.draw do
 
   root :to => 'pages#home'
 
+  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
