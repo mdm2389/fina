2411ac11f314ad3ef1cb8c1d744911f679d4e1e8
Melissa Meade <mdm2389@gmail.com>
2013-06-13 20:51:38 -0400

    Modifications Remove Password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,22 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
+  def edit 
+  	@user = current_user 
+  end
+
+ def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end 
+ end 
+
 end
